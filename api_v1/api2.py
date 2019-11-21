9f4dcfdec683eb54f28a28fcec81e99942a7e532
walkiop <53264853+walkiop@users.noreply.github.com>
2019-11-21 15:19:17 -0300

    Update api2.py

--- api_v1/api2.py
+++ api_v1/api2.py
@@ -1,1 +1,64 @@
-import
+import json
+import traceback
+import base64
+import logging
+from datetime import date, datetime
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'senhaFib'
+app.config['MYSQL_DATABASE_DB'] = 'fibdb'
+app.config['MYSQL_DATABASE_HOST'] = 'mysql'
+
+mysql.init_app(app)
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+def teste():
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from fibonacci")
+    print cursor, str(cursor)
+    r = [dict((cursor.Valor[i][0], value)
+              for i, value in enumerate(row)) for row in cursor.fetchall()]
+    json_string = json.dumps(r, default=json_serial)
+    print json_string
+    return "hola"
+
+def Segundo():
+    data_e_hora_atuais = datetime.now()
+    data_e_hora_em_texto = data_e_hora_atuais.strftime('%S')
+    return int(data_e_hora_em_texto)
+
+@app.route("/")
+def hello():
+    return "Benvido a API FIAP!\n"
+
+@app.route("/Teste")
+def Teste():
+    return "Welcome to teste!\n"
+
+@app.route("/getDados")
+def getDados():
+    try:
+        cursor = mysql.connect().cursor()
+        sSql = "SELECT * from fibonacci where " + Segundo()
+        cursor.execute(sSql)
+        
+        r = [dict((cursor.Valor[i][0], value)
+            for i, value in enumerate(row)) for row in cursor.fetchall()]
+        json_string = json.dumps(r, default=json_serial)
+        return json_string
+    except Exception as e:
+        return 'Erro /getDados' + str(e) + traceback.format_exc()
+
+if __name__ == "__main__":
+    #teste()
+    app.run(host='0.0.0.0',debug=True)
