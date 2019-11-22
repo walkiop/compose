3025d689abeba53b6aaf62a1ffb232da65150cff
walkiop <53264853+walkiop@users.noreply.github.com>
2019-11-22 14:32:45 -0300

    Update api.py

--- api_v1/api.py
+++ api_v1/api.py
@@ -1,1 +1,18 @@
-#
+
+# Connect to the database
+connection = pymysql.connect(host='mysql',
+                             user='root',
+                             password='senhaFiap',
+                             db='fiapdb',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT posicao, valor FROM fibonacci WHERE posicao = %s"
+        cursor.execute(sql, 5)
+        result = cursor.fetchone()
+        print(result)
+finally:
+    connection.close()
