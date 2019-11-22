8123172ae9c3efbb144f37a2509b90f77505cd1f
walkiop <53264853+walkiop@users.noreply.github.com>
2019-11-22 14:09:03 -0300

    Update api.py

--- api_v1/api.py
+++ api_v1/api.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import json
 import traceback
 import base64
@@ -10,10 +11,10 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'senhaFib'
-app.config['MYSQL_DATABASE_DB'] = 'fibdb'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'senhaFiap'
+app.config['MYSQL_DATABASE_DB'] = 'fiapdb'
 app.config['MYSQL_DATABASE_HOST'] = 'mysql'
-
+#app.config['CORS_HEADERS'] = 'Content-Type'
 mysql.init_app(app)
 
 def json_serial(obj):
@@ -22,38 +23,30 @@ def json_serial(obj):
         return obj.isoformat()
     raise TypeError ("Type %s not serializable" % type(obj))
 
-#def teste():
-#    cursor = mysql.connect().cursor()
-#    cursor.execute("SELECT * from fibonacci")
-#    print cursor, str(cursor)
-#    r = [dict((cursor.Valor[i][0], value)
-#              for i, value in enumerate(row)) for row in cursor.fetchall()]
-#    json_string = json.dumps(r, default=json_serial)
-#    print json_string
-#    return "hola"
-
-def Segundo():
-    data_e_hora_atuais = datetime.now()
-    data_e_hora_em_texto = data_e_hora_atuais.strftime('%S')
-    return int(data_e_hora_em_texto)
+def teste():
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * from Aso")
+    print cursor, str(cursor)
+    r = [dict((cursor.description[i][0], value)
+              for i, value in enumerate(row)) for row in cursor.fetchall()]
+    json_string = json.dumps(r, default=json_serial)
+    print json_string
+    return "hola"
 
 @app.route("/")
 def hello():
     return "Benvido a API FIAP!\n"
 
-#@app.route("/Teste")
-#def Teste():
-#    return "Welcome to teste!\n"
+@app.route("/Teste")
+def Teste():
+    return "Welcome to teste!\n"
 
 @app.route("/getDados")
 def getDados():
     try:
         cursor = mysql.connect().cursor()
-        #sSql = "SELECT * from fibonacci where posicao = " + Segundo()
-        #cursor.execute(sSql)
-        cursor.execute("SELECT * from fibonacci")
-        
-        r = [dict((cursor.Valor[i][0], value)
+        cursor.execute("SELECT * from Aso")
+        r = [dict((cursor.description[i][0], value)
             for i, value in enumerate(row)) for row in cursor.fetchall()]
         json_string = json.dumps(r, default=json_serial)
         return json_string
